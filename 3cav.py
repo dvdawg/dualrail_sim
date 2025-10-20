b4835e8a1635303a1b472b4a7bffea942886592c
David Kim <davidkm@berkeley.edu>
2025-10-20 14:51:46 -0700

    fixed random issues

--- 3cav.py
+++ 3cav.py
@@ -2,10 +2,10 @@ import qutip as qt
 
 def dual_rail_H(
     N1=10, N2=10, Nc=20,
-    w1=5.0, w2=5.2, wc=7.0,           # ω_λ
-    k1=-0.25, k2=-0.25, kc=0.0,       # Kerr α_λ (renamed: k*)
-    g1=0.050, g2=0.050,               # g_j
-    g12=0.010                         # g_12
+    w1=5.0, w2=5.2, wc=7.0,
+    k1=-0.25, k2=-0.25, kc=0.0,
+    g1=0.050, g2=0.050,
+    g12=0.010
 ):
 
     a1_op = qt.destroy(N1)
@@ -14,13 +14,13 @@ def dual_rail_H(
 
     I1, I2, Ic = qt.qeye(N1), qt.qeye(N2), qt.qeye(Nc)
 
-    b1  = qt.tensor(a1_op, I2, Ic)
-    b2  = qt.tensor(I1, a2_op, Ic)
-    bc  = qt.tensor(I1, I2, ac_op)
+    b1 = qt.tensor(a1_op, I2, Ic)
+    b2 = qt.tensor(I1, a2_op, Ic)
+    bc = qt.tensor(I1, I2, ac_op)
 
     b1d, b2d, bcd = b1.dag(), b2.dag(), bc.dag()
 
-    H_loc  = w1 * (b1d * b1) + (k1/2.0) * (b1d * b1d * b1 * b1)
+    H_loc = w1 * (b1d * b1) + (k1/2.0) * (b1d * b1d * b1 * b1)
     H_loc += w2 * (b2d * b2) + (k2/2.0) * (b2d * b2d * b2 * b2)
     H_loc += wc * (bcd * bc)
     if kc != 0.0:
